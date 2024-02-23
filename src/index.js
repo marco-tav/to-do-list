8fcea5d0736617ac5e74aa512dbd8be47fee3616
marco-tav <marco.tavella@outlook.com>
2024-02-23 17:11:53 -0300

    Add HTML template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles.css';
+import gitHubIcon from './assets/images/github-mark.svg';
+import PlexSansBold1 from './assets/fonts/IBMPlexSans-Bold.woff';
+import PlexSansBold2 from './assets/fonts/IBMPlexSans-Bold.woff2';
+import PlexSansSemiBold1 from './assets/fonts/IBMPlexSans-SemiBold.woff';
+import PlexSansSemiBold2 from './assets/fonts/IBMPlexSans-SemiBold.woff2';
+import PlexSerif1 from './assets/fonts/IBMPlexSerif-Regular.woff';
+import PlexSerif2 from './assets/fonts/IBMPlexSerif-Regular.woff2';
